105e9fc0bba7d6a32b99e507b8daa272462f5876
BritishPandaChick <sbartley1080@gmail.com>
2018-10-20 18:11:50 -0400

    Update code files and README file

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# liz-lemon
+# Liz Lemon Website
+Skillcrush 102 project. Made with HTML, CSS, JavaScript, and jQuery.
 
-Skillcrush 102 Liz Lemon Project
-Made with HTML, CSS, JavaScript, and jQuery
+## Instructions
+Clone or download zip file. Open index.html file in your browser.
+
+## Contributing
+This repository is based on a design created by Skillcrush. Therefore I will not be accepting any pull requests.
+
+## Acknowledgements
+* Skillcrush
--- script.js
+++
@@ -1,78 +0,0 @@
-$(document).ready(function(){
-  /*  alert('Our JavaScript is working!');
-  console.log('Our JavaScript is working!');
-  console.warn('A dire warning!');
-  console.error('ERMAGERD AN ERROR!');  */
-
-  var modalContainer = $("#modal-container");
-  var hideModal = function(){
-    modalContainer.hide();
-  };
-
-  var showModal = function() {
-    modalContainer.show();
-  };
-
-  var modalShowButton = $("#modal-show");
-  modalShowButton.on("click", showModal);
-  var modalCloseButton = $('#modal-hide');
-  modalCloseButton.on("click", hideModal);
-
-  $(document).on("keyup", function(evt) {
-    evt = evt || window.event;
-    if (evt.keycode === 27) {
-      hideModal();
-    }
-  });
-
-  var handleNewsletterSignup = function(evt) {
-    evt.preventDefault();
-    var newsletterHeader = $("#newsletter-header");
-    var newsletterForm = $("#newsletter-signup");
-    newsletterForm.hide();
-    newsletterHeader.text("Thank you for signing up!");
-    setTimeout(hideModal, 2000);
-  };
-
-  var newsletterForm = $("#newsletter-signup");
-  newsletterForm.on("submit", handleNewsletterSignup);
-
-  /* Begin the clock code */
-  var clockTime = function() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-
-    if (hours <= 11) {
-      var period = "AM";
-    } else {
-      var period = "PM";
-    }
-
-    if (hours > 12) {
-       hours = hours - 12;
-     } else if (hours === 0) {
-       hours = 12;
-     }
-
-     if (minutes < 10) {
-       minutes = "0" + String(minutes);
-     }
-
-     if (seconds < 10) {
-       seconds = "0" + String(seconds);
-     }
-
-     var time = hours + ':' + minutes + ':' + seconds + ' ' + period;
-     return time;
-   }
-
-   var clock = $("#clock");
-
-   setInterval(function() {
-     clock.text(clockTime());
-   }, 1000);
-
-  
- });
