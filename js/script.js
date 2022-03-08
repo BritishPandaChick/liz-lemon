616ace81c73395350826b7e28fa886428b00ebe0
BritishPandaChick <sbartley1080@gmail.com>
2022-03-08 15:27:09 -0500

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,62 +1,78 @@
-//Modal
-const modalContainer = document.querySelector("#modal-container");
-const hideModal = function() {
+/*  Modal code */
+var modalContainer = document.querySelector("#modal-container");
+
+// Hides Modal
+var hideModal = function() {
   modalContainer.hide();
-};
-const showModal = function() {
+};
+
+// Shows modal 
+var showModal = function() {
   modalContainer.show();
-};
-const modalShowButton = $("#modal-show");
-modalShowButton.on("click", showModal);
-const modalCloseButton = $('#modal-hide');
-modalCloseButton.on("click", hideModal);
+};
+
+var modalShowButton = $("#modal-show");
+modalShowButton.on("click", showModal());
+
+var modalCloseButton = $('#modal-hide');
+modalCloseButton.on("click", hideModal());
 
 $(document).on("keyup", function(evt) {
   evt = evt || window.event;
   if (evt.keycode === 27) {
     hideModal();
   }
-});
-const handleNewsletterSignup = function(evt) {
+});
+
+/* === Newsletter Sign up === */
+var handleNewsletterSignup = function(evt) {
   evt.preventDefault();
-  const newsletterHeader = $("#newsletter-header");
-  const newsletterForm = $("#newsletter-signup");
+  var newsletterHeader = $("#newsletter-header");
+  var newsletterForm = $("#newsletter-signup");
+
   newsletterForm.hide();
   newsletterHeader.text("Thank you for signing up!");
   setTimeout(hideModal, 2000);
-};
-const newsletterForm = $("#newsletter-signup");
-newsletterForm.on("submit", handleNewsletterSignup);
+};
 
-/* Begin the clock code */
-const clockTime = function() {
-  let currentTime = new Date();
-  let hours = currentTime.getHours();
-  let minutes = currentTime.getMinutes();
-  let seconds = currentTime.getSeconds();
+var newsletterForm = $("#newsletter-signup");
+newsletterForm.on("submit", handleNewsletterSignup());
+
+/* === Begin the clock code === */
+var clockTime = function() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
 
   if (hours <= 11) {
-    let period = "AM";
+    var period = "AM";
   } else {
-    let period = "PM";
-  }
+    var period = "PM";
+  }
+
   if (hours > 12) {
     hours = hours - 12;
   } else if (hours === 0) {
     hours = 12;
-  }
+  }
+
   if (minutes < 10) {
    minutes = "0" + String(minutes);
   }
 
   if (seconds < 10) {
     seconds = "0" + String(seconds);
-  }
-  let time = hours + ':' + minutes + ':' + seconds + ' ' + period;
-    return time;
-  }
-  let clock = $("#clock");
+  }
 
-  setInterval(function() {
-    clock.text(clockTime());
-  }, 1000);
+  var time = hours + ':' + minutes + ':' + seconds + ' ' + period;
+    return time;
+}
+
+var clock = $("#clock");
+
+setInterval(function() {
+  clock.text(clockTime());
+}, 1000);
+
+
